2db26ce90e4754a843e6b5f80f0fd42573e6b9f4
Aditya_prajapati <114338023+prajapati-aditya@users.noreply.github.com>
2025-10-31 15:05:23 +0530

    Update power of a number.py

--- 14-10-25/Recursion_1/power of a number.py
+++ 14-10-25/Recursion_1/power of a number.py
@@ -1,30 +1,25 @@
-class Solution:
-    def reverseexponentiation(self, n):
-        
-        # power calc using iteration
-        def power(base , n ):
-            res = 1 
-            while n > 0 :
-                if n % 2 == 1:
-                    res = res * base
+    # using iteration
+def power(base , n ):
+    res = 1 
+    while n > 0 :
+        if n % 2 == 1:
+            res = res * base
                 
-                base *= base
-                n = n//2
-            return res
+        base *= base
+        n = n//2
+    return res
         
         # power calc using recursion
-        def pow_recr(base,exp) :
-            if exp == 0 :
-                return 1
-            if n < 0 :
-                return 1 / pow_recr(base, -exp) 
+def pow_recr(base,exp) :
+    if exp == 0 :
+        return 1
+    if n < 0 :
+        return 1 / pow_recr(base, -exp) 
         
-        base  = n
-        # calculating reverse of the number
-        rev = 0
-        while n != 0 :
-            rem = n % 10
-            rev = rev*10 + rem
-            n =  n // 10
+    half = power(base, exp//2)
+    if exp % 2 == 0 :
+        return half* half
+    else :
+        return base*half*half
         
-        return power(base , rev)
+    return power(base , rev)
